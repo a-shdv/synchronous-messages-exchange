c521c952ef41b6985a368034366ddc9f1ed9d80d
a-shdv <shadaev2001@icloud.com>
2023-12-17 02:26:34 +0400

    Add deployment

--- aggregator-api/src/main/java/com/company/model/Page.java
+++ aggregator-api/src/main/java/com/company/model/Page.java
@@ -1,4 +1,11 @@
 package com.company.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pages")
 public class Page {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 }
